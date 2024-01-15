3b993f52d8deda8667ca8e4c0fc978c907a8df7a
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-01-15 14:11:46 +0900

    add Layout.js

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+
+
 export default function FirstPost() {
 	return (
 		<div>
+			<Head>
+				<title>First Post</title>
+			</Head>
 			<h1>最初の投稿</h1>
-			<h2>ホームへ戻る</h2>
+			<Link href="/"><a>ホームへ戻る</a></Link>
+
 		</div>
 	);
 }
